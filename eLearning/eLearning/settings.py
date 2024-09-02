8cfd837e87c69e7715bf4489907e88686c933b89
Veronica Minutillo <95313241+glodean19@users.noreply.github.com>
2024-09-02 15:47:16 +0100

    Update settings.py

--- eLearning/eLearning/settings.py
+++ eLearning/eLearning/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Crispy forms settings for rendering Bootstrap 5 forms
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
